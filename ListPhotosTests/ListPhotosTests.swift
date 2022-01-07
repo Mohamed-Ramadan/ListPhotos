7e80f9f94e2bd69626eaf43cf849608b71f5263b
Mohamed.Ramadan_ios <ramadan@isafte-land.com>
2022-01-07 16:24:40 +0200

    Initial Commit

---
+++ ListPhotosTests/ListPhotosTests.swift
@@ -0,0 +1,33 @@
+//
+//  ListPhotosTests.swift
+//  ListPhotosTests
+//
+//  Created by Mohamed Ramadan on 07/01/2022.
+//
+
+import XCTest
+@testable import ListPhotos
+
+class ListPhotosTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}
