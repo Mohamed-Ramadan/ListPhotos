e89529ca68297921a62b2d55b1d2fb5f0179c462
Mohamed.Ramadan_ios <ramadan@isafte-land.com>
2022-01-07 16:44:36 +0200

    Add fetch photos apis to networkService

--- ListPhotos/Data/Remote/Network/NetworkService.swift
+++ ListPhotos/Data/Remote/Network/NetworkService.swift
@@ -15,7 +15,7 @@ class URLSessionNetworkService: NetworkService {
      
     func getPhotos(request: PhotosRequestDTO, completion: @escaping (Result<PhotosResponseDTO, Error>) -> Void) {
         
-        let urlString = Constants.serverBaseURl + "page=\(request.page)&limit=\(request.limit)"
+        let urlString = Constants.serverBaseURl + "v2/list?page=\(request.page)&limit=\(request.limit)"
         guard let urlEncodedString = urlString.addingPercentEncoding(withAllowedCharacters: .urlFragmentAllowed), let url = URL(string: urlEncodedString) else {
             print("Wrong URL!: \(urlString)")
             return
